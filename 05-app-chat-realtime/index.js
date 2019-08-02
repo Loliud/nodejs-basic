75321fad41cf2088101592b523286114ca6936fa
Loliud <anhhaiquynhon123@gmail.com>
2019-08-02 16:43:54 +0700

    login app chat

--- 05-app-chat-realtime/index.js
+++ 05-app-chat-realtime/index.js
@@ -4,7 +4,22 @@ const server = require('http').createServer(app);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
+const io = require('socket.io')(server);
 
+
+let users = ['ken'];
+
+io.on('connection', (socket) =>{
+    console.log(socket.id + ': connected');
+    socket.on('create-user', (data) =>{
+        if(checkUser(data, users) === false){
+            socket.emit('create-user-fail', 'User da bi trung');
+        }else{
+            users.push(data);
+            socket.broadcast.emit('newUser-connected', data);
+        }
+    });
+});
 
 app.get('/', (req, res) =>{
     res.render('home');
@@ -12,3 +27,14 @@ app.get('/', (req, res) =>{
 
 
 server.listen(8000);
+
+
+let checkUser = (user, listUser) =>{
+    let key = true;
+    listUser.forEach((item) =>{
+        if(item === user){
+            key = false;
+        }
+    });
+    return key;
+}
--- 05-app-chat-realtime/public/chat.js
+++ 05-app-chat-realtime/public/chat.js
@@ -1,9 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
 
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.getElementById('body');
     const createUser = document.getElementById('create-user');
     const login = document.getElementById('login');
     const contentBox = document.getElementById('content');
     const username = document.getElementById('username');
+    const boxChat = document.getElementById('form-chat');
 
     username.addEventListener('change', () =>{
 
@@ -11,9 +15,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
     login.addEventListener('click', () => {
         contentBox.setAttribute('style', 'display:flex');
+        socket.emit('create-user', username.value);
         createUser.remove();
+
+        
 
 
     });
+
+    socket.on('create-user-fail', (data) =>{
+        alert(data, 'Hay tao user khac');
+        body.appendChild(createUser);
+        contentBox.setAttribute('style', 'display:none');
+    });
+
+    socket.on('newUser-connected', (data) =>{
+        let noti = document.createElement('div');
+        noti.innerHTML = `${data} connected`;
+        boxChat.appendChild(noti);
+    })
+
 });
 
